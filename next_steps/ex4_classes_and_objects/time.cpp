f8ce632bdcb02fe3f91b0a51d4ba3c52b65dbc0c
Nicola Gatto <nicola.gatto@rwth-aachen.de>
2019-09-12 16:18:20 +0200

    Next Steps: Example 4

---
+++ next_steps/ex4_classes_and_objects/time.cpp
@@ -0,0 +1,62 @@
+// time.cpp, Maggie Johnson
+// Description: A simple time class.
+
+#include <iostream>
+using namespace std;
+
+class Time {
+ private:
+  int hours_;
+  int minutes_;
+  int seconds_;
+ public:
+  void set(int h, int m, int s) {hours_ = h; minutes_ = m; seconds_ = s; return;}
+  void increment();
+  void decrement();
+  void display();
+};
+
+void Time::increment() {
+  seconds_++;
+  minutes_ += seconds_/60;
+  hours_ += minutes_/60;
+  seconds_ %= 60;
+  minutes_ %= 60;
+  hours_ %= 24;
+  return;
+}
+
+void Time::decrement() {
+    seconds_--;
+    if (seconds_ < 0) {
+        seconds_ = 59;
+        minutes_ -= 1;
+    }
+
+    if (minutes_ < 0) {
+        minutes_ = 59;
+        hours_ -= 1;
+    }
+
+    if (hours_ < 0) {
+        hours_ = 23;
+    }
+    return;
+}
+
+void Time::display() {
+  cout << (hours_ % 12 ? hours_ % 12:12) << ':'
+       << (minutes_ < 10 ? "0" :"") << minutes_ << ':'
+       << (seconds_ < 10 ? "0" :"") << seconds_
+       << (hours_ < 12 ? " AM" : " PM") << endl;
+}
+
+int main() {
+  Time timer;
+  timer.set(23,2,58);
+  for (int i = 0; i < 300; i++) {
+    timer.decrement();
+    timer.display();
+    cout << endl;
+  }
+}
